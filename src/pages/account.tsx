3ed6015180b14293865973e10a8c0e38b21507fb
amy <ctrlalternativedelete@gmail.com>
2022-10-09 03:26:40 +1100

    fixed up styling on overflow

--- src/pages/account.tsx
+++ src/pages/account.tsx
@@ -82,7 +82,7 @@ const Account: NextPage = () => {
                         { sessions.data ? 
                             sessions.data?.map(e => (
                                 <tr key={e.id} className="border">
-                                    <td><pre className="overflow-x-scroll whitespace-nowrap">{e.sessionToken}</pre></td>
+                                    <td><pre className="overflow-x-auto whitespace-nowrap">{e.sessionToken}</pre></td>
                                     <td>{e.expires.toLocaleDateString()}</td>
                                     <td><a className="underline" href={`steam://run/1058830//play token ${e.sessionToken}`}>Load</a></td>
                                 </tr>
