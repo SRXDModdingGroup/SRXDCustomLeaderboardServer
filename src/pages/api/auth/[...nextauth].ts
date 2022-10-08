f415c0378af5b5c25ec1132c2f33b27a8a47ff5d
amy <ctrlalternativedelete@gmail.com>
2022-10-08 21:33:40 +1100

    added setting verified if email verified on discor

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -9,12 +9,31 @@ import { env } from "../../../env/server.mjs";
 export const authOptions: NextAuthOptions = {
     // Include user.id on session
     callbacks: {
+        async signIn({account, profile, user}) {
+            console.log(account, profile, user)
+            if (account.provider === "discord" && profile.verified && !user.emailVerified && user.id !== account.providerAccountId) {
+                try {
+                    await prisma.user.update({
+                        where: {
+                            id: user.id
+                        },
+                        data: {
+                            emailVerified: new Date()
+                        }
+                    })
+                }
+                catch(e) {
+                    console.warn(e)
+                }
+            }
+            return true
+        },
         session({ session, user }) {
             if (session.user) {
                 session.user.id = user.id;
             }
             return session;
-        },
+        },        
     },
     // Configure one or more authentication providers
     adapter: PrismaAdapter(prisma),
@@ -22,6 +41,21 @@ export const authOptions: NextAuthOptions = {
         DiscordProvider({
             clientId: env.DISCORD_CLIENT_ID,
             clientSecret: env.DISCORD_CLIENT_SECRET,
+            profile(profile) {
+                if (profile.avatar === null) {
+                    const defaultAvatarNumber = parseInt(profile.discriminator) % 5
+                    profile.image_url = `https://cdn.discordapp.com/embed/avatars/${defaultAvatarNumber}.png`
+                } else {
+                    const format = profile.avatar.startsWith("a_") ? "gif" : "png"
+                    profile.image_url = `https://cdn.discordapp.com/avatars/${profile.id}/${profile.avatar}.${format}`
+                }
+                return {
+                    id: profile.id,
+                    name: profile.username,
+                    email: profile.email,
+                    image: profile.image_url
+                }
+            },
         }),
     // ...add more providers here
     ],
