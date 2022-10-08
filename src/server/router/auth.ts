ba02cb4696fca23f322f01170c29fc9127a0ff5e
amy <ctrlalternativedelete@gmail.com>
2022-10-09 03:29:36 +1100

    fixed up update user type

--- src/server/router/auth.ts
+++ src/server/router/auth.ts
@@ -20,7 +20,9 @@ export const authRouter = createRouter()
                 where: {
                     id: ctx.user.id
                 },
-                data: input
+                data: {
+                    ...input
+                } as any
             });
         }
     })
